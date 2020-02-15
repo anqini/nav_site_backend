1cfc303c731a5e7f79d8787140c890bfb1303904
Anqi Ni <angelo43023@outlook.com>
2020-02-15 13:43:38 -0600

    delete model

--- src/routes/website.ts
+++ src/routes/website.ts
@@ -6,8 +6,6 @@ import ITag from "../interfaces/tag";
 import ITableRow from "../interfaces/tableInfo";
 // mongoose model
 import Website from "../models/website";
-import Tag from "../models/tag";
-import TableRow from "../models/tableRow";
 
 /**
  * @class WAPI
@@ -79,7 +77,7 @@ export class WAPI {
     public getSchools(req: Request, res: Response, next: NextFunction) {
 
         // get Website
-        Tag.find({}, {rank: 1, title: 1}).then((tags:  Array<ITag>) => {
+        Website.find({}, {rank: 1, title: 1}).then((tags:  Array<ITag>) => {
             // verify website is found
             if (!tags.length) {
                 res.sendStatus(404);
@@ -102,7 +100,7 @@ export class WAPI {
     public getTableRows(req: Request, res: Response, next: NextFunction) {
 
         // get Website
-        TableRow.find({}, {title: 1, rankIn2020: 1, rankIn2019: 1, state: 1, logo: 1, 
+        Website.find({}, {title: 1, rankIn2020: 1, rankIn2019: 1, state: 1, logo: 1, 
         EnglishName: 1, change1: 1, change2: 1}).then((tags:  Array<ITableRow>) => {
             // verify website is found
             if (!tags.length) {
